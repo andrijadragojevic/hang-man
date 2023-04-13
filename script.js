b963806a43e945b5a54a733e3801ca0f7c8a9e80
andrijadragojevic <andrijadrago55@gmail.com>
2023-04-13 15:49:45 +0200

    changed image loading to lazy

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ function game(result) {
 
 
   document.querySelector("#odgovor>h1").innerHTML = shownResult;
-  document.getElementById("hangman-image").innerHTML = `<img src="img/${mistakes}.png" alt="">`
+  document.getElementById("hangman-image").innerHTML = `<img src="img/${mistakes}.png" alt="" loading="lazy">`
 
   //  Generating buttons
   abeceda.forEach(letter => {
